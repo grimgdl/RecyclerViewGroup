1f0065982962d67907a450b57de8cb74eee07e56
Gustavo Ibarra <gustavoibarra1989@gmail.com>
2022-12-03 21:35:43 -0600

    fixed issues

--- Recyclergroup/src/main/java/com/grimco/recyclergroup/recycler/data/Brand.kt
+++ Recyclergroup/src/main/java/com/grimco/recyclergroup/recycler/data/Brand.kt
@@ -1,3 +1,3 @@
 package com.grimco.recyclergroup.recycler.data
 
-data class Brand( val name: String, var products: List<Product>, val img: String? = null, val id:Int = 0)
+data class Brand( val name: String, var products: List<Product>?, val img: String? = null, val id:Int = 0)
